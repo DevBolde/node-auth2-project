2062e5ffead97c1bca8ff3594596816fa4fd5401
Devin Bolden <devinbolden6@gmail.com>
2023-09-21 21:13:21 -0500

    wip

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -17,7 +17,19 @@ function find() {
         "role_name": "instructor"
       }
     ]
-   */
+
+    /**
+     * select
+        user_id,
+        username,
+        role_name
+      from users 
+        join roles on 
+          users.role_id = roles.role_id;
+     */
+   return db('users')
+   .joins('roles', 'users.role_id', 'roles.role_id')
+   .select('user_id', 'username', 'role_name')
 }
 
 function findBy(filter) {
@@ -33,8 +45,23 @@ function findBy(filter) {
         "role_name": "admin",
       }
     ]
+
+    select
+        user_id,
+        username,
+        password,
+        role_name
+    from users 
+    join roles on 
+        users.role_id = roles.role_id
+    where users.user_id = 1;
    */
-}
+    return db('users')
+    .joins('roles', 'users.role_id', 'roles.role_id')
+    .select('user_id', 'username','password', 'role_name')
+    .where(filter)
+  }
+    
 
 function findById(user_id) {
   /**
